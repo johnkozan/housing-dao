078183ceb63863711d0aa4434db5e6de7c19a9cd
Anar Enhsaihan <aenhsaihan@gmail.com>
2020-02-15 19:54:41 -0700

    Remove referredBy bug

--- contracts/Hourglass.sol
+++ contracts/Hourglass.sol
@@ -270,8 +270,8 @@ contract Hourglass {
     /**
      * Converts all incoming ethereum to tokens for the caller, and passes down the referral addy (if any)
      */
-    function buy(address _referredBy) public payable returns (uint256) {
-        purchaseTokens(msg.value, _referredBy);
+    function buy() public payable returns (uint256) {
+        purchaseTokens(msg.value, address(0));
     }
 
     /**
