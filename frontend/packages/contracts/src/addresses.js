377152a61f2dbc91f556f6276bc1a59033454990
John Kozan <john@kozan.sk.ca>
2020-02-15 19:31:01 -0600

    basic token landing page, buy/sell works

---
+++ frontend/packages/contracts/src/addresses.js
@@ -0,0 +1,7 @@
+// This address points to a dummy ERC20 contract deployed on Ethereum Mainnet,
+// Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
+const addresses = {
+  token: "0x12FC7882Fb52c73fCa405a08A3E9a681761D1fDD",
+};
+
+export default addresses;
